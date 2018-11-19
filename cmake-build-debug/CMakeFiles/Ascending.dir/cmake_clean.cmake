8cb0ff7ef89eb96d5749923683a97951e7276783
prosenjitkumar <prosenjitearnkumar@gmail.com>
2018-11-19 13:17:13 +0600

    Ascending-and-descending-with C

---
+++ cmake-build-debug/CMakeFiles/Ascending.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Ascending.dir/main.c.o"
+  "Ascending.pdb"
+  "Ascending"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Ascending.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
